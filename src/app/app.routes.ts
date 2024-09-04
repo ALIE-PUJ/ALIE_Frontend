29aeddf72e312ba3a1d51c1f52d5de7468458a61
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-04 15:46:49 -0500

    Base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './contents/homepage/homepage.component';
+import { LoginComponent } from './contents/login/login.component';
+import { NotfoundComponent } from './contents/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+    { path: 'homepage', component: HomepageComponent },
+    // Ruta comodín para manejar rutas no existentes
+    { path: 'PageNotFound', component: NotfoundComponent },
+    { path: '**', redirectTo: '/PageNotFound' },
+    { path: 'login', component: LoginComponent },
+];
