2e99c05761ceebbe0621e44f3b4dcb56516f4a9b
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-04 15:48:03 -0500

    HomePage (Rename)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomepageComponent } from './contents/homepage/homepage.component';
+import { HomePageComponent } from './contents/home-page/home-page.component';
 import { LoginComponent } from './contents/login/login.component';
 import { PageNotFoundComponent } from './contents/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-    { path: 'homepage', component: HomepageComponent },
+    { path: 'homepage', component: HomePageComponent },
     // Ruta comodín para manejar rutas no existentes
     { path: 'PageNotFound', component: PageNotFoundComponent },
     { path: '**', redirectTo: '/PageNotFound' },
