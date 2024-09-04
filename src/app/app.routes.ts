1859cefa60bb0e08b82de938527e08836a62c368
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-04 15:59:54 -0500

    Rename (Contents -> Pages)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './contents/home-page/home-page.component';
-import { LoginComponent } from './contents/login/login.component';
-import { PageNotFoundComponent } from './contents/page-not-found/page-not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/homepage', pathMatch: 'full' }, // Pagina por defecto
     { path: 'homepage', component: HomePageComponent }, // Pagina por defecto
+    { path: 'login', component: LoginComponent }, // Login (Tanto para administradores como para estudiantes)
+
+
+    // Siempre debe ir al final, para evitar errores en la redireccion de rutas si existentes
     { path: 'PageNotFound', component: PageNotFoundComponent }, // Ruta comodín para manejar rutas no existentes
     { path: '**', redirectTo: '/PageNotFound' }, // Pagina no encontrada (Rutas no existentes)
-    { path: 'login', component: LoginComponent }, // Login (Tanto para administradores como para estudiantes)
 ];
