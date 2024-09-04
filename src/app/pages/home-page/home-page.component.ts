4dbf9933575322d158551da729f7675ee187c6cd
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-04 18:02:45 -0500

    Footer implemented in home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DefaultFooterComponent } from '../../layout/default-footer/default-footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [DefaultFooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
