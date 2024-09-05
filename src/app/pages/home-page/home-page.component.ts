19cff33c7b6b48485f5d075b4f7653307d0a60df
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-05 01:21:46 -0500

    Base homepage fix

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DefaultFooterComponent } from '../../layout/default-footer/default-footer.component';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [DefaultFooterComponent],
+  imports: [DefaultFooterComponent, RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
