cf227354ffffafca2b6c20024d6fbf67e8b960c4
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-05 01:36:14 -0500

    Role management for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { DefaultFooterComponent } from '../../layout/default-footer/default-footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultFooterComponent],
+  imports: [DefaultFooterComponent, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor() { }
+
+  handleLogin() {
+    const roleSelect = (document.getElementById('roleToggle') as HTMLSelectElement).value;
+    
+    if (roleSelect === 'Student') {
+      this.loginAsStudent();
+    } else if (roleSelect === 'Admin') {
+      this.loginAsAdmin();
+    }
+  }
+
+  loginAsStudent() {
+    localStorage.setItem('ActiveRole', 'Student');
+    // Add your login logic for student here
+    alert('Logged in as Student');
+  }
+
+  loginAsAdmin() {
+    localStorage.setItem('ActiveRole', 'Admin');
+    // Add your login logic for admin here
+    alert('Logged in as Admin');
+  }
 
 }
