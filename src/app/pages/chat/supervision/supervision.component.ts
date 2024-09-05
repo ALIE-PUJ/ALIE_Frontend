41210835a436ae9435a4f0013a11d7e19a8200fc
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-04 21:54:02 -0500

    Sidebar implemented into supervision

--- src/app/pages/chat/supervision/supervision.component.ts
+++ src/app/pages/chat/supervision/supervision.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-supervision',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './supervision.component.html',
   styleUrl: './supervision.component.scss'
 })
