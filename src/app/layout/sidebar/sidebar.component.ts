4e267ae03ded3219a481335a7872f91033521e01
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-05 01:45:54 -0500

    Active role management (Sidebar access control)

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -13,13 +13,30 @@ import { RouterModule } from '@angular/router'; // Import RouterModule
 })
 export class SidebarComponent {
 
+  // Sidebar Variables
   // Get user role from input. Can be Student or Admin
   @Input() userRole: string = 'Admin';
-
   isCollapsed = false;
 
+  // Inicializacion
+  ngOnInit() {
+    this.verificarAcceso()
+  }
+
+
+  // Funciones auxiliares
+
+  // Comprimir o expandir la sidebar
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  // Control de acceso
+  verificarAcceso(): void {
+    // Recuperar el rol activo del usuario que inicio sesion, desde localStorage
+    const storedActiveRole = localStorage.getItem('ActiveRole');
+    this.userRole = storedActiveRole ? storedActiveRole : 'Student'; // Estudiante por defecto
+    console.log("[SIDEBAR]: Active Role = ", this.userRole);
   }
 
 }
