ca0a90c2495fa09c1cca00ca7d01f3f1d2b8f6d3
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-09-04 23:23:05 -0500

    Sidebar buttons fix

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, RouterModule
   ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
