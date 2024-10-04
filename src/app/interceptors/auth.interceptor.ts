16687b5bd55763ca79c5b8405e7dfd3bdf725045
JuanCSUCoder <jcsu2019369@gmail.com>
2024-10-04 13:47:46 -0500

    Feat: Refactor interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -6,17 +6,15 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   if (typeof localStorage === 'undefined') {
     console.log('[authInterceptor] Skipping in server-side rendering');
     return next(req);
-  }
-
-  if (localStorage.getItem('token') === null) {
+  } else if (localStorage.getItem('token') === null) {
     console.log('[authInterceptor] No token found in localStorage');
     return next(req);
+  } else {
+    console.log('[authInterceptor] Adding Authorization header to request');
+    const reqAuth = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + localStorage.getItem('token') || '')
+    });
+
+    return next(reqAuth);
   }
-
-  console.log('[authInterceptor] Adding Authorization header to request');
-  const reqAuth = req.clone({
-    headers: req.headers.set('Authorization', 'Bearer ' + localStorage.getItem('token') || '')
-  });
-
-  return next(reqAuth);
 };
