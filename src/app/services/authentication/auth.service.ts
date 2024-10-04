118afb66babf63e5845fbe864377b01474db1242
JuanCSUCoder <jcsu2019369@gmail.com>
2024-10-04 13:00:25 -0500

    Fix: Imports

--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { User } from '../../model/User';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
