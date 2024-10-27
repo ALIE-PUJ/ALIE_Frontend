42496b3be4e4b753bec82780e9e5c462ebd2852b
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-10-27 05:13:11 -0500

    Clickable links in messages

--- src/app/pages/chat/agent-chat/agent-chat.component.ts
+++ src/app/pages/chat/agent-chat/agent-chat.component.ts
@@ -884,5 +884,9 @@ toggleChatsView() {
     });
   }
 
+  convertUrlsToLinks(text: string): string {
+    const urlPattern = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,\.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(urlPattern, '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>');
+  }
 
 }
